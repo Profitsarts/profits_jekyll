15366f2f2a1a0a008ee23ad6ca668699980e6e59
profitsarts <profitsarts@gmail.com>
2025-07-18 19:47:05 +0200

    fixed devtools 25

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,88 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var shell       = require('gulp-shell');
+var cp = require('child_process');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('jekyll-watch', shell.task(['jekyll build --watch --incremental']));
+var messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ bundle exec jekyll build'
+};
 
-gulp.task('copy_bower_components', function() {
-    gulp.src([
-        'bower_components/bootstrap/dist/css/bootstrap.min.css',
-        'bower_components/bootstrap/dist/css/bootstrap-theme.min.css',
-        'bower_components/fontawesome/css/font-awesome.min.css',
-        'bower_components/normalize.css/normalize.css'
-        ]).pipe(gulp.dest('./_site/vendor/css/'));
-    gulp.src([
-        'bower_components/bootstrap/dist/fonts/*',
-        'bower_components/fontawesome/fonts/*',
-        ]).pipe(gulp.dest('./_site/vendor/fonts/'));
-    gulp.src([
-        'bower_components/bootstrap/dist/js/bootstrap.min.js',
-        'bower_components/jquery/dist/jquery.min.js',
-        ]).pipe(gulp.dest('./_site/vendor/js/'));
-    gulp.src([
-        'bower_components/modernizr/modernizr.js',
-        ]).pipe(uglify())
-        .pipe(gulp.dest('./_site/vendor/js/'));
+// Jekyll build task (usa bundle exec SIEMPRE para gems correctas)
+gulp.task('jekyll-build', function(done) {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn('bundle', ['exec', 'jekyll', 'build'], { stdio: 'inherit' })
+    .on('close', done);
 });
 
-gulp.task('webserver', function () {
-    browserSync.init({
-        port: 4000,
-        open: true,
-        reloadThrottle: 500,
-        reloadDebounce: 500,
-        server: {
-            baseDir: '_site/'
-        }
-    });
-    setTimeout(function () {
-        gulp.watch('_site/**/*.*').on('change', browserSync.reload);
-    }, 5000);
-})
+// SASS Compilation
+gulp.task('sass-rebuild', function() {
+  var plugins = [autoprefixer(), cssnano()];
+  return gulp.src('assets/css/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(gulp.dest('_site/assets/css/'))
+    .pipe(browserSync.stream());
+});
 
-gulp.task('serve', ['jekyll-watch', 'copy_bower_components', 'webserver'], function () {});
+// JS Task (simple copy, add minify if needed)
+gulp.task('js-rebuild', function() {
+  return gulp.src('assets/js/**/*.js')
+    .pipe(gulp.dest('_site/assets/js/'))
+    .pipe(browserSync.stream());
+});
+
+// Copy images (for development)
+gulp.task('images-rebuild', function() {
+  return gulp.src('assets/img/**/*.*')
+    .pipe(gulp.dest('_site/assets/img/'))
+    .pipe(browserSync.stream());
+});
+
+// Manual optimization of images (when you want)
+gulp.task('imagemin', function() {
+  return gulp.src('assets/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('assets/img/'));
+});
+
+// BrowserSync config for /profits_jekyll routing
+gulp.task('serve', ['jekyll-build', 'sass-rebuild', 'js-rebuild', 'images-rebuild'], function() {
+  browserSync.init({
+    port: 3000,
+    server: {
+      baseDir: './',
+      routes: {
+        '/profits_jekyll': '_site'
+      }
+    },
+    startPath: '/profits_jekyll/'
+  });
+
+  gulp.watch('assets/css/**/*.scss', ['sass-rebuild']);
+  gulp.watch('assets/js/**/*.js', ['js-rebuild']);
+  gulp.watch('assets/img/**/*.*', ['images-rebuild']);
+  gulp.watch([
+      '**/*.*',
+      '!_site/**/*',
+      '!assets/css/**/*',
+      '!assets/js/**/*',
+      '!assets/img/**/*',
+      '!node_modules/**/*'
+  ], ['jekyll-rebuild']);
+});
+
+// This task rebuilds Jekyll and reloads BrowserSync
+gulp.task('jekyll-rebuild', ['jekyll-build'], function(done) {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('default', ['serve']);
